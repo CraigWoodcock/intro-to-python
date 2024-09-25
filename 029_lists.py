fa420e786871fdf145135b9a68043d15a2d6a5b4
Craig Woodcock <cwoodcock@spartaglobal.com>
2024-09-26 00:27:20 +0100

    lists, list indexing

--- 029_lists.py
+++ 029_lists.py
@@ -25,7 +25,7 @@ my_friends = ["Victoria", "Mel", "Melanie", "Emma"]
 
 # @TASK Try making your own here:
 
-your_list = ...
+your_list = ["Craig", 35, "Emma", 33]
 print(your_list)
 
 # @TASK and print it out by running:
