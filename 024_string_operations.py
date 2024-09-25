52c47b1a4bee8c6423faec9a3a8027a7027b2470
Craig Woodcock <cwoodcock@spartaglobal.com>
2024-09-25 23:55:12 +0100

    string operations

--- 024_string_operations.py
+++ 024_string_operations.py
@@ -15,7 +15,7 @@ from lib.helpers import check_that_these_are_equal
 
 # Here's an example:
 
-length = len("Hello!")
+length = len("My name is Craig")
 print(f"The string is {length} characters long")
 
 # @TASK: Try it out yourself by changing the string "Hello!"
@@ -42,7 +42,7 @@ old_string = "Hello, YOUR_NAME!"
 new_string = old_string.replace("YOUR_NAME", "Kay")
 
 # Uncomment this next line to see the result
-# print(new_string)
+print(new_string)
 
 # You'll notice here that the function is coming in a
 # different place. Let's compare `len` and `replace`:
@@ -80,7 +80,8 @@ print("Function: uppercase")
 
 def make_uppercase(string):
   # Return the string in uppercase
-  pass
+  
+  return string.upper()
 
 check_that_these_are_equal(
   make_uppercase("hello"), "HELLO")
@@ -97,7 +98,8 @@ print("Function: lowercase")
 
 def make_lowercase(string):
   # Return the string in lowercase
-  pass
+  lowercase = string.lower()
+  return lowercase
 
 check_that_these_are_equal(
   make_lowercase("HELLO"), "hello")
@@ -115,7 +117,8 @@ print("Function: strip_whitespace")
 def strip_whitespace(string):
   # Return the string with any whitespace removed from
   # the start and end
-  pass
+  without_whitespace = string.strip()  # in java we use .trim()
+  return without_whitespace
 
 check_that_these_are_equal(
   strip_whitespace("hello "), "hello")
