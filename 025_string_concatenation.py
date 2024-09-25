84516a7496d72aa9eb11a9790d81b3c13a0b39cf
Craig Woodcock <cwoodcock@spartaglobal.com>
2024-09-26 00:01:24 +0100

    string concatenation and fstrings

--- 025_string_concatenation.py
+++ 025_string_concatenation.py
@@ -58,7 +58,8 @@ print("Function: greet")
 def greet(name):
 	# Return the string "Hello, Kay!" where "Kay" is the
 	# name provided
-	pass
+	greeting = (f"Hello, {name}!")
+	return greeting
 
 check_that_these_are_equal(
 	greet("Chuang-tzu"),
